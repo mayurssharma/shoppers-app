e1dda742d55c9bbf9777d4865c6342945e11fa97
mayurssharma <mayurssharma99@gmail.com>
2024-02-22 16:45:34 +0530

    Chnges & Add content in it

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+export const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="logo" />
+                <p>SHOPPER</p>
+            </div>
+            <ul className="nav-menu">
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kid</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="" />
+            </div>
+        </div>
+    )
+}
